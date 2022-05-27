9b3195411b5e71dbeb425e75533d44168832fe9d
Вадим Павличенко <Vadymchyk111@gmail.com>
2022-05-27 16:42:50 +0300

    FixedCode

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -12,24 +12,24 @@ namespace Task7
         {
             int priceOfCrystall = 5;
             int playerMoney;
-            int playerCrystall=0;
-            int countOfCrystallToBuy;
+            int playerCrystalls=0;
+            int countOfCrystallsToBuy;
 
             Console.WriteLine("Введите количество денег");
             playerMoney = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Введите количество кристалов для покупки");
-            countOfCrystallToBuy = Int32.Parse(Console.ReadLine());
-            if(playerMoney < priceOfCrystall * countOfCrystallToBuy)
+            countOfCrystallsToBuy = Int32.Parse(Console.ReadLine());
+            if(playerMoney < priceOfCrystall * countOfCrystallsToBuy)
             {
                 Console.WriteLine("У вас недостаточно денег");
                 return;
             }
             else
             {
-                playerMoney -= priceOfCrystall * countOfCrystallToBuy;
-                playerCrystall += countOfCrystallToBuy;
+                playerMoney -= priceOfCrystall * countOfCrystallsToBuy;
+                playerCrystalls += countOfCrystallsToBuy;
             }
-            Console.WriteLine("У вас осталось " + playerMoney + " монет и "+ playerCrystall+" кристаллов");
+            Console.WriteLine("У вас осталось " + playerMoney + " монет и "+ playerCrystalls + " кристаллов");
         }
     }
 }
